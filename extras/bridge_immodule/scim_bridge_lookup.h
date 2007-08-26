3b81a368a90efd64b3900f14968dc3cfd6f9efa8
Ryo Dairiki <ryo-dairiki@mbm.nifty.com>
2007-08-26 08:44:49 +0000

    - Begin to import scim-bridge as an extra immodule.
    svn commit r50

---
+++ extras/bridge_immodule/scim_bridge_lookup.h
@@ -0,0 +1,41 @@
+/**
+ * @file scim_lookup_table.h
+ * @brief Definition of LookupTable classes.
+ */
+
+/*
+ * Smart Common Input Method
+ * 
+ * Copyright (c) 2002-2005 James Su <suzhe@tsinghua.org.cn>
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA  02111-1307  USA
+ */
+ 
+#ifndef SCIM_BRIDGE_LOOKUP_H_
+#define SCIM_BRIDGE_LOOKUP_H_
+
+#include <stdlib.h>
+
+#include "scim_bridge_attribute.h"
+#include "scim_bridge_types.h"
+
+/**
+ * Data struct of lookup tables.
+ */
+typedef struct _ScimLookupTable ScimLookupTable;
+
+#endif /*SCIM_BRIDGE_LOOKUP_H_*/
