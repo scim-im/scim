51d809a9f8c586ef42e2552626bf1b98c5166716
Tz-Huan Huang <tzhuan@gmail.com>
2012-09-22 01:07:27 +0800

    Inialize pointer member data. Thanks to NetBSD.

--- extras/immodules/agent/scim-bridge-agent.cpp
+++ extras/immodules/agent/scim-bridge-agent.cpp
@@ -198,6 +198,7 @@ ScimBridgeAgent *ScimBridgeAgent::alloc ()
 
 ScimBridgeAgentImpl::ScimBridgeAgentImpl ():
 running (true), noexit_enabled (false), standalone_enabled (false), client_app_count (0),
+scim_config_module(0),
 accept_listener (NULL), interruption_listener (NULL), panel_listener (NULL), signal_listener (NULL), display (NULL)
 {
 }
